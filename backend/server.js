c64029f5b1f145b9a99b7400dfab6daef24e7cbd
Gindadwi <gindadwi144@gmail.com>
2024-08-17 09:46:20 +0700

    payment gateway firebase midtrans

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const midtransClient = require('midtrans-client');
+const cors = require('cors')({ origin: true }); // Atur `origin` sesuai dengan kebutuhan
 
 let coreApi = new midtransClient.CoreApi({
     isProduction: false,
@@ -7,28 +8,30 @@ let coreApi = new midtransClient.CoreApi({
     clientKey: 'SB-Mid-client-tN5lJrODMKqfbgfh'
 });
 
-exports.createTransaction = functions.https.onRequest(async (req, res) => {
-    try {
-        const { orderDetails, user, totalAmount } = req.body;
+exports.createTransaction = functions.https.onRequest((req, res) => {
+    cors(req, res, async () => {
+        try {
+            const { orderDetails, user, totalAmount } = req.body;
 
-        let parameter = {
-            "payment_type": "gopay", // Or any other payment method
-            "transaction_details": {
-                "order_id": `order-${Math.floor(Math.random() * 1000000)}`,
-                "gross_amount": totalAmount
-            },
-            "customer_details": {
-                "first_name": orderDetails.nama,
-                "phone": orderDetails.nomor,
-                "address": orderDetails.alamat,
-                "email": user.email
-            }
-        };
+            let parameter = {
+                "payment_type": "gopay",
+                "transaction_details": {
+                    "order_id": `order-${Math.floor(Math.random() * 1000000)}`,
+                    "gross_amount": totalAmount
+                },
+                "customer_details": {
+                    "first_name": orderDetails.nama,
+                    "phone": orderDetails.nomor,
+                    "address": orderDetails.alamat,
+                    "email": user.email
+                }
+            };
 
-        const transaction = await coreApi.charge(parameter);
-        res.json({ transaction });
-    } catch (error) {
-        console.error('Error creating transaction:', error);
-        res.status(500).send('Transaction creation failed');
-    }
+            const transaction = await coreApi.charge(parameter);
+            res.json({ transaction });
+        } catch (error) {
+            console.error('Error creating transaction:', error);
+            res.status(500).send('Transaction creation failed');
+        }
+    });
 });
